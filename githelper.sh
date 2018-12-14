fa772459a8913519fb8c54eb10d4982afc001779
Rocky <rockycool000007@gmail.com>
2018-12-14 21:00:00 +0000

    Updated the code

--- githelper.sh
+++ githelper.sh
@@ -8,7 +8,8 @@ echo " Enter one of the following options :
 	5. Create new branch
 	6. Clone the code"
 
-V_PROJECT_PATH="/opt/scripts/session1-git/"
+V_INIT_PATH="/opt/code/"
+V_PROJECT_PATH="/opt/code/session1-git/"
 V_REPO_REFERENCE="origin"
 
 read V_OPTION
@@ -18,15 +19,30 @@ echo "$V_OPTION"
 # If else loop for the option user inputs
 if [ "$V_OPTION" -eq "1" ]; then
 	echo "Initializing git repository"
+	cd "$V_INIT_PATH"
 	git init
 
 elif [ "$V_OPTION" -eq "2" ]; then
 	echo "Enter Branch Name : " 
 	read V_BRANCH_NAME
 	echo "Pulling latest code from ${V_BRANCH_NAME}"
-	# git --git-dir=/opt/scripts/session1-git/.git pull origin "$V_BRANCH_NAME"
-	cd /opt/scripts/session1-git/
+	# git --git-dir=/opt/code/session1-git/.git pull origin "$V_BRANCH_NAME"
+	cd "$V_PROJECT_PATH"
 	git pull origin "$V_BRANCH_NAME"
+
+elif [ "$V_OPTION" -eq 3 ]; then
+	echo "Confirm to push the code : (Y/N)"
+	read V_CONFIRM_PUSH
+	if [ "$V_CONFIRM_PUSH" = "Y" ]; then
+		echo "Enter message to commit the code : "
+		read V_COMMIT_MESSAGE
+		echo "Enter branch name : "
+		read V_BRANCH_NAME
+		cd "$V_PROJECT_PATH"
+		git add *
+		git commit -m "$V_COMMIT_MESSAGE"
+		git push -u "$V_REPO_REFERENCE" "$V_BRANCH_NAME" 
+	fi
 
 elif [ "$V_OPTION" -eq "5" ]; then
 	echo "Enter the new branch name : "
@@ -44,6 +60,7 @@ elif [ "$V_OPTION" -eq "5" ]; then
 elif [ "$V_OPTION" -eq "6" ]; then
 	echo "Enter the repository url to clone: "
 	read V_REPO_URL
+	cd "$V_INIT_PATH"
 	git clone "$V_REPO_URL"
 	echo The repository has been cloned from "$V_REPO_URL"
 fi
