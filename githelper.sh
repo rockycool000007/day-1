13037013a8723d0864e8a63ce707b3fd62801d08
Rocky <rockycool000007@gmail.com>
2018-12-14 19:54:58 +0000

    Add script to automate git tasks

---
+++ githelper.sh
@@ -0,0 +1,55 @@
+!#/bin/bash
+
+echo " Enter one of the following options : 
+        1. Initialize git 
+	2. Pull the latest code
+	3. Push the code to remote repository
+	4. Merge the code
+	5. Create new branch
+	6. Clone the code"
+
+V_PROJECT_PATH="/opt/scripts/session1-git/"
+V_REPO_REFERENCE="origin"
+
+read V_OPTION
+echo "$V_OPTION"
+#echo "${V_OPTION}" works too
+
+# If else loop for the option user inputs
+if [ "$V_OPTION" -eq "1" ]; then
+	echo "Initializing git repository"
+	git init
+
+elif [ "$V_OPTION" -eq "2" ]; then
+	echo "Enter Branch Name : " 
+	read V_BRANCH_NAME
+	echo "Pulling latest code from ${V_BRANCH_NAME}"
+	# git --git-dir=/opt/scripts/session1-git/.git pull origin "$V_BRANCH_NAME"
+	cd /opt/scripts/session1-git/
+	git pull origin "$V_BRANCH_NAME"
+
+elif [ "$V_OPTION" -eq "5" ]; then
+	echo "Enter the new branch name : "
+	read V_NEW_BRANCH
+	cd "$V_PROJECT_PATH"
+	git branch "$V_NEW_BRANCH"
+	git push "$V_REPO_REFERENCE" "$V_NEW_BRANCH"
+	echo "Do you want to checkout the new branch ? (Y/N) "
+	read V_CHECKOUT_BRANCH_OPT
+	
+	if [ "$V_CHECKOUT_BRANCH_OPT" = "Y" ]; then
+		git checkout "$V_NEW_BRANCH"
+	fi
+
+elif [ "$V_OPTION" -eq "6" ]; then
+	echo "Enter the repository url to clone: "
+	read V_REPO_URL
+	git clone "$V_REPO_URL"
+	echo The repository has been cloned from "$V_REPO_URL"
+fi
+
+
+# Check if the operation succeeded using the exit code
+if [ "$?" -eq "0" ]; then
+	echo "The operation was successful"
+fi
